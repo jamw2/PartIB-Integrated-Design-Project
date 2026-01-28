461561346eb34b83a014620a3acaffee3d854a99
Tejas Gharat <tng24@cam.ac.uk>
2026-01-28 10:12:19 +0000

    servos

---
+++ sw/test_adc.py
@@ -0,0 +1,16 @@
+from machine import ADC, Pin
+from utime import sleep
+
+
+def test():
+    # Set the LED pin and configuration
+    adc_pin = 28
+    adc = ADC(Pin(adc_pin))
+
+    # Continiously update the LED value and print said value
+    while True:
+        adc_value = adc.read_u16()
+        dist = adc_value  # * 5200 / 65535
+        # print distance in mm
+        print(dist)
+        sleep(1)
--- sw/test_pwm.py
+++ sw/test_pwm.py
@@ -1,24 +1,26 @@
 from machine import Pin, PWM
 from utime import sleep
 
-def test_pwm():
-    pwm_pin_no = 28  # Pin 28 = GP28 (labelled 34 on the jumper)
-    pwm_pin = PWM(Pin(pwm_pin_no), 100)
 
-    level = 0  # 0-100
-    direction = 1  # 1=up, -1=down
+def test_pwm():
+    pwm_pin_no = 15  # Pin 15  gp13
+    pwm_pin = PWM(Pin(pwm_pin_no))
+    pwm_pin.freq(50)
+
+    level = 15  # 0-100
+    direction = -1  # 1=up, -1=down
 
     while True:
         # PWM the specified pin
-        u16_level = int(65535 * level / 100)
+        u16_level = int(16000 * level / 100)
         pwm_pin.duty_u16(u16_level)
-    
+
         # update level and sleep
         print(f"Level={level}, u16_level={u16_level}, direction={direction}")
         level += direction
-        if level == 100:
+        if level == 50:
             direction = -1
-        elif level == 0:
+        elif level == 10:
             direction = 1
         sleep(0.1)
 
